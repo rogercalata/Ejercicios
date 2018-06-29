105ef912ba51c0f1ce71ccff7d18d563bc014edd
rogercalata <35268126+rogercalata@users.noreply.github.com>
2018-06-29 14:45:59 +0200

    resolucion mapper
    ejemplo base hacer mapping

--- Mapper/Program.cs
+++ Mapper/Program.cs
@@ -14,34 +14,46 @@ namespace Mapper
             public int id { get; set; }
             public string Nombre { get; set; }
             public string Apellidos { get; set; }
-            public int edad { get; set; }
+            public string sexo { get; set; }
+            public DateTime fechaN { get; set; }
         }
         public class forma2
         {
             public int id { get; set; }
-            public string Nombre { get; set; }
-            public string Apellidos { get; set; }
+            public string NombreComp { get; set; }
+            public int edad { get; set; }
             public DateTime fechaN { get; set; }
+
         }
         public class EJmapper
         {
             static void Main(string[] args)
             {
-                var conf = new MapperConfiguration(cfg => cfg.CreateMap<forma1, forma2>());
-                IMapper iMapper = conf.CreateMapper();
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<forma1, forma2>()
+                ///Se encarga de juntar el nombre y apellido del primer modelo y lo pasa a nombre completo en el segundo.
+                 .ForMember(dest => dest.NombreComp, sou => sou.ResolveUsing(entity => entity.Nombre + "" + entity.Apellidos))
+                 ///se encarga de transformar la fecha de nacimento en la edad
+                 .ForMember(dest => dest.edad, sou => sou.ResolveUsing(entity => DateTime.Today.AddTicks(-entity.fechaN.Ticks).Year - 1))
+                 /// Se encarga de ignorar el registro de fecha de nacimento en el modelo2 ya que lo transformamos a edad
+                 .ForMember(dest => dest.fechaN, opt=> opt.Ignore()));
 
-                var source = new forma1();
-                source.id = 1;
-                source.Nombre = "Roger";
-                source.Apellidos = "Calatayud";
-                source.edad = 23;
+                IMapper iMapper = config.CreateMapper();
+
+                var source = new forma1 {
+                    id = 1,
+                    Nombre = "Roger",
+                    Apellidos = "Calatayud",
+                    sexo = "Hombre",
+                    fechaN = new DateTime(1994, 9, 28)
+            };
+
                 var destino = iMapper.Map<forma1, forma2>(source);
-                
                 Console.WriteLine(destino.GetType());
-                Console.WriteLine("Nombre de autor" + destino.Nombre + " " + destino.Apellidos + " " + destino.fechaN);
+                Console.WriteLine("Nombre completo: " + destino.NombreComp + "\nEdad: " + destino.edad);
                 Console.ReadLine();
-
+                
             }
         }
     }
 }
+
